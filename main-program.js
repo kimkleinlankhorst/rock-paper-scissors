ac5d81cdfbe744a92fe067694f28e019d86b8975
Kim <kimkl@home.nl>
2026-01-29 15:44:46 +0100

    Computer choice funtion added
    Returns a random value for the computer's choice

--- main-program.js
+++ main-program.js
@@ -1,1 +1,15 @@
 console.log("TEST");
+
+function getComputerChoice() {
+    var randomNr = Math.random();
+
+    if (randomNr < 0.33) {
+        return "rock"
+    } else if (randomNr < 0.66) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
