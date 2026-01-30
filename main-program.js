31a0b9186338ba39ca449306eb78e39652402237
Kim <kimkl@home.nl>
2026-01-30 15:01:57 +0100

    Working game
    On loading the webpage, through a prompt five rounds can be played against the computer

--- main-program.js
+++ main-program.js
@@ -1,4 +1,5 @@
-console.log("TEST");
+var humanScore = 0;
+var computerScore = 0;
 
 function getComputerChoice() {
     var randomNr = Math.random();
@@ -12,4 +13,61 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    return prompt("Type your choice here (rock, paper or scissors)").toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            return "It's a draw"
+        } else if (computerChoice === "paper") {
+            computerScore++
+            return "You lose, paper beats rock"
+        } else {
+            humanScore++
+            return "You win, rock beats scissors"
+        }
+    }
+
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanScore++
+            return "You win, paper beats rock"
+        } else if (computerChoice === "paper") {
+            return "It's a draw"
+        } else {
+            computerScore++
+            return "You lose, scissors beat paper"
+        }
+    }
+
+    if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            computerScore++
+            return "You lose, rock beats scissors"
+        } else if (computerChoice === "paper") {
+            humanScore++
+            return "You win, scissors beat paper"
+        } else {
+            return "It's a draw"
+        }
+    }
+
+    return "A false value was entered, please choose out of rock, paper or scissors"
+
+}
+
+function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        console.log("ROUND " + (i + 1))
+        console.log(playRound(getHumanChoice(), getComputerChoice()))
+        console.log("SCORE -> You: " + humanScore + ", Computer: " + computerScore);
+    }
+}
+
+playGame()
