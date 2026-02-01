b2f0c0787b3d65e16893d7db4dd5a91a2eea9537
Kim <kimkl@home.nl>
2026-02-01 14:30:15 +0100

    First to five can now be played through a basic UI

--- main-program.js
+++ main-program.js
@@ -18,14 +18,15 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    var resultStr = ""
 
     if (humanChoice === "rock") {
         if (computerChoice === "rock") {
-            return "It's a draw"
+            return "It's a draw, you both chose rock"
         } else if (computerChoice === "paper") {
             computerScore++
             return "You lose, paper beats rock"
-        } else {
+        } else if (computerChoice === "scissors") {
             humanScore++
             return "You win, rock beats scissors"
         }
@@ -36,8 +37,8 @@ function playRound(humanChoice, computerChoice) {
             humanScore++
             return "You win, paper beats rock"
         } else if (computerChoice === "paper") {
-            return "It's a draw"
-        } else {
+            return "It's a draw, you both chose paper"
+        } else if (computerChoice === "scissors") {
             computerScore++
             return "You lose, scissors beat paper"
         }
@@ -50,24 +51,66 @@ function playRound(humanChoice, computerChoice) {
         } else if (computerChoice === "paper") {
             humanScore++
             return "You win, scissors beat paper"
-        } else {
-            return "It's a draw"
+        } else if (computerChoice === "scissors") {
+            return "It's a draw, you both chose scissors"
         }
     }
 
-    return "A false value was entered, please choose out of rock, paper or scissors"
+    if (resultStr === "") {
+        resultStr = "A false value was entered, please choose out of rock, paper or scissors"
+    }
+
+    return resultStr;
 
 }
 
-function playGame() {
-    humanScore = 0;
-    computerScore = 0;
+// function playGame() {
+//     humanScore = 0;
+//     computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        console.log("ROUND " + (i + 1))
-        console.log(playRound(getHumanChoice(), getComputerChoice()))
-        console.log("SCORE -> You: " + humanScore + ", Computer: " + computerScore);
+//     for (let i = 0; i < 5; i++) {
+//         console.log("ROUND " + (i + 1))
+//         console.log(playRound(getHumanChoice(), getComputerChoice()))
+//         console.log("SCORE -> You: " + humanScore + ", Computer: " + computerScore);
+//     }
+// }
+
+// playGame()
+
+function createScoreText() {
+    if (humanScore <= 4 && computerScore <= 4) {
+        return "SCORE = You: " + humanScore + ", Computer: " + computerScore;
+    } else if (humanScore > 4) {
+        humanScore = 0;
+        computerScore = 0;
+        return "You won! Now you can start a new game, first to five wins"
+    } else if (computerScore > 4) {
+        humanScore = 0;
+        computerScore = 0;
+        return "The computer won, Now you can start a new game, first to five wins"
     }
 }
 
-playGame()
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+
+const resultDiv = document.querySelector(".result");
+const scoreDiv = document.querySelector(".score");
+
+scoreDiv.textContent = createScoreText();
+
+rockButton.onclick = () => {
+    resultDiv.textContent = (playRound("rock", getComputerChoice()));
+    scoreDiv.textContent = createScoreText();
+};
+paperButton.onclick = () => {
+    resultDiv.textContent = (playRound("paper", getComputerChoice()))
+    scoreDiv.textContent = createScoreText();
+};
+scissorsButton.onclick = () => {
+    resultDiv.textContent = (playRound("scissors", getComputerChoice()))
+    scoreDiv.textContent = createScoreText();
+};
+
+
